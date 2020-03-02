0cffb3ccaee2eb74e70d8b254bf49ff47292954a
Ruslan <r.salikhov96@gmail.com>
2020-03-02 23:44:12 +0500

    feat: add solution brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  module.exports = function check(str, bracketsConfig) {
+      for (let i = 0; i < bracketsConfig.length;) {
+          let config = bracketsConfig[i].join('');
+          if (str.includes(config)) {
+              str = str.replace(config, '');
+              i = 0;
+          } else {
+              i++;
+          }
+      }
+
+      return str.length != 0 ? false : true;
+  }
